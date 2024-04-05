68844bae2386d14457c8beda21e3be54f57326c2
raptech-jp <paparukisan@gmail.com>
2024-04-06 00:22:38 +0900

    [add: analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react"
 import type { Metadata } from "next";
 import { Noto_Sans_JP } from 'next/font/google'
 import "./globals.css";
@@ -20,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="jp">
       <body className={notoSansJP.className}>{children}</body>
+      <Analytics />
     </html>
   );
 }
